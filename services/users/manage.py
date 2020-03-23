27727742bccb2db8ff63d4a31e4a5a68c5a6ca37
Lucas Helal <lucasghelal@gmail.com>
2020-03-23 01:02:49 -0300

    populating db

--- services/users/manage.py
+++ services/users/manage.py
@@ -25,5 +25,12 @@ def test():
         return 0
     sys.exit(result)
 
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
 if __name__ == '__main__':
     cli()
