ee97508b7d1e0ce7b85347090aa610f16117f4c1
Lucas Helal <lucasghelal@gmail.com>
2020-03-26 00:59:05 -0300

    Code Coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,10 +2,20 @@ import sys
 import unittest
 
 from flask.cli import FlaskGroup
+import coverage 
 
 from project import create_app, db
 from project.api.models import User
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -31,6 +41,21 @@ def seed_db():
     db.session.add(User(username='michael', email="hermanmu@gmail.com"))
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
