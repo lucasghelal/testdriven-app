c91faa97609fbe231c42a34d3dde9e35f94c66b5
Lucas Helal <lucasghelal@gmail.com>
2020-04-02 16:13:53 -0300

    React Routes

--- services/client/src/App.js
+++ services/client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
+import About from './components/About';
+
 
 
 class App extends Component {
@@ -55,16 +58,26 @@ class App extends Component {
           <div className="columns">
             <div className="column is-half">  
               <br/>
-              <h1 className="title is-1">All Users</h1>
-              <hr/><br/>
-              <AddUser 
-                username={this.state.username}
-                email={this.state.email}
-                addUser={this.addUser}
-                handleChange={this.handleChange}
-              />  
-              <hr/><br/>  
-              <UsersList users={this.state.users}/>
+              <Switch>
+                <Route exact path='/'render={() => (
+                  <div>
+                    <h1 className="title is-1">All Users</h1>
+                    <hr/><br/>
+                    <AddUser 
+                      username={this.state.username}
+                      email={this.state.email}
+                      addUser={this.addUser}
+                      handleChange={this.handleChange}
+                    />  
+                    <hr/><br/>  
+                    <UsersList users={this.state.users}/>
+                    <hr/><br/>
+                  </div>
+                )} />
+               
+                <Route exact path='/about' component={About}/>
+              </Switch>
+            
             </div>
           </div>
         </div>
